3ae7a41afea6868423fabac0d625b231a61dd986
Ryan Winch <stoneyentertainment@gmail.com>
2011-11-16 02:20:00 -0500

    upload

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Use the non-open-source part, if present
+-include vendor/htc/pyramid/BoardConfigVendor.mk
+
+# Use the part that is common between all pyramids
+#include device/htc/pyramid/BoardConfig.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := pyramid
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := msm8260
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_HAVE_HDMI_OUT := true
+TARGET_USES_OVERLAY := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_HAVE_TSLIB := false
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+#Wifi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+
+#Audio 
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := true
+
+#Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_EGL_CFG := device/htc/pyramid/egl.cfg
+
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+#BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+
+#BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+BOARD_USES_ADRENO_200 := true
+#BOARD_NO_RGBX_8888 := true
+
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
+
+TARGET_BOOTLOADER_BOARD_NAME := pyramid
+
+BOARD_USE_NEW_LIBRIL_HTC := true
+TARGET_PROVIDES_LIBRIL := vendor/htc/pyramid/proprietary/libril.so
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_BASE := 0x40400000
+BOARD_PAGE_SIZE := 2048
+
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := pyramid
+#BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+TARGET_HAVE_HDMI_OUT := true
+TARGET_HARDWARE_3D := true
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00fffc00
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x31fffc00
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x4aabc400
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/htc/pyramid/kernel
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_CUSTOM_GRAPHICS:= ../../../device/htc/pyramid/graphics.c
+BOARD_USES_RECOVERY_CHARGEMODE := true
+
+BOARD_USES_QCOM_LIBS := true
